6eba71b546546b34400dcfecb2133179cd343c97
Rails Designer <devs@railsdesigner.com>
2025-11-03 12:56:27 +0700

    Update page title using turbo stream broadcasts

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,13 @@
 class Message < ApplicationRecord
+  broadcasts :messages, inserts_by: :prepend
+
+  after_commit :set_title_counter
+
   def title = "Message #{id}"
+
+  private
+
+  def set_title_counter
+    broadcast_action_to :messages, action: :set_title_counter, attributes: { count: Message.count }
+  end
 end
